4aaf3bb94e9cd2461be728beb64f5a9613e87865
Claudiu Ionita <ClaudiuI@thelotter.com>
2019-12-10 17:28:33 +0200

    added docker image, changes

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module go-examples
 
 go 1.13
 
-require github.com/gorilla/mux v1.7.3
+require (
+	github.com/go-redis/redis v6.15.6+incompatible
+	github.com/gorilla/mux v1.7.3
+	github.com/joho/godotenv v1.3.0
+	github.com/streadway/amqp v0.0.0-20190827072141-edfb9018d271
+)
--- main.go
+++ main.go
@@ -1,20 +1,67 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 
+	"github.com/go-redis/redis"
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+	"github.com/streadway/amqp"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World!")
-}
+var isEnvSet bool = true
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		isEnvSet = false
+	}
+
+	if isEnvSet {
+		ConnectRedis()
+		ConnectRabbitMQ()
+	}
+
 	router := mux.NewRouter()
 	router.HandleFunc("/", hello)
 	log.Println("Server running")
 	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	_ = json.NewEncoder(w).Encode(struct {
+		Text     string
+		IsEnvSet bool
+	}{
+		Text:     "Hello World!",
+		IsEnvSet: isEnvSet,
+	})
+}
+
+func ConnectRedis() {
+	db, _ := strconv.Atoi(os.Getenv("REDIS_DB"))
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_HOST"),
+		Password: os.Getenv("REDIS_PASSWORD"),
+		DB:       db,
+	})
+
+	_, err := rdb.Ping().Result()
+	if err != nil {
+		panic(err)
+	}
+	log.Println("Redis connected")
+}
+
+func ConnectRabbitMQ() {
+	conn, err := amqp.Dial(os.Getenv("AMQP_HOST"))
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
+	log.Println("RabbitMQ connected")
 }
